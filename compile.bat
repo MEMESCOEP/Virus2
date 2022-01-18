24072336f3af4ae7be0eb8d6f202d0a246a3018a
Andrew Maney <andrewmaney05@gmail.com>
2022-01-18 11:26:57 -0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,14 @@
 @echo off
 echo Building...
 pyinstaller --noconfirm --onefile --windowed .\lmao.pyw
-echo Done.
+if %ERRORLEVEL% NEQ 0 goto ERR
+goto EOF
+
+
+:ERR
+echo ERROR!!!!!! >>>:((
+pause
+
+:EOF
+echo Done. Exitting in 5 seconds...
+timeout 5 > NUL
