48df6128850c6e95d755dd6b7e63d9c78743f756
Andrew Maney <andrewmaney05@gmail.com>
2022-01-22 19:38:13 -0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building...
-pyinstaller --noconfirm --onefile --windowed .\lmao.pyw
+pyinstaller --exclude matplotlib --exclude scipy --exclude pandas --noconfirm --onefile --windowed .\lmao.pyw
 if %ERRORLEVEL% NEQ 0 goto ERR
 goto EOF
 
@@ -10,5 +10,10 @@ echo ERROR!!!!!! >>>:((
 pause
 
 :EOF
+move .\dist\*.exe .\
+rmdir /s /q build
+rmdir /s /q dist
+rmdir /s /q __pycache__
+pause
 echo Done. Exitting in 5 seconds...
 timeout 5 > NUL
